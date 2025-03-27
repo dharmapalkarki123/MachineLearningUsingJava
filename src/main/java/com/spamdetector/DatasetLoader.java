aa5e166c7697760390c82e263df9ff958e78ce96
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-03-27 21:30:11 +0545

    commit

--- src/main/java/com/spamdetector/DatasetLoader.java
+++ src/main/java/com/spamdetector/DatasetLoader.java
@@ -1,5 +1,34 @@
 package com.spamdetector;
 
-public class DatasetLoader {
+import java.io.IOException;
+import java.io.File;
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
 
+public class DatasetLoader {
+	 public Instances loadDataset(String filePath) throws IOException {
+	        // Load the CSV file
+	        CSVLoader loader = new CSVLoader();
+	        loader.setSource(new File(filePath));
+
+	        // Convert to WEKA dataset format
+	        Instances data = loader.getDataSet();
+
+	        // Set the class index (the last column is usually the label)
+	        data.setClassIndex(data.numAttributes() - 1);
+
+	        return data;
+	    }
+
+	    public static void main(String[] args) {
+	        try {
+	            DatasetLoader loader = new DatasetLoader();
+	            Instances dataset = loader.loadDataset("src/main/resources/dataset/spam.csv");
+	            System.out.println("Dataset loaded successfully!");
+	            System.out.println("Number of Instances: " + dataset.numInstances());
+	            System.out.println("Number of Attributes: " + dataset.numAttributes());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
